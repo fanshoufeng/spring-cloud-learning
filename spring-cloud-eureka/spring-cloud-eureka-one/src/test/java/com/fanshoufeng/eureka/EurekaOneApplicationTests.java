408c5c4157bf3f7c3d1e3d733851c10c07267e14
Michael Fan <shoufeng.fan@gmail.com>
2020-06-07 16:59:03 +0800

    新增spring-cloud-eureka-one

---
+++ spring-cloud-eureka/spring-cloud-eureka-one/src/test/java/com/fanshoufeng/eureka/EurekaOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanshoufeng.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
