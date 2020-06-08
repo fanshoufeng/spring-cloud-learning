ab96c47ddee024bc2157545eeed534bd3e0a31b0
Michael Fan <shoufeng.fan@gmail.com>
2020-06-08 21:35:12 +0800

    新增consumer

---
+++ eureka-producer-consumer/consumer/src/main/java/com/fanshoufeng/consumer/remote/HelloRemote.java
@@ -0,0 +1,13 @@
+package com.fanshoufeng.consumer.remote;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name= "producer")
+public interface HelloRemote {
+
+    @RequestMapping(value = "/hello")
+    String hello(@RequestParam(value = "name") String name);
+
+}
