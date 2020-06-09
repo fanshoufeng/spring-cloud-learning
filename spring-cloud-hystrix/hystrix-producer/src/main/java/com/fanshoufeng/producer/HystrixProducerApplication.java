355547a09819b94da3b3623d698ca8a2f02b84e3
Michael Fan <shoufeng.fan@gmail.com>
2020-06-09 21:41:03 +0800

    新增hystrix-producer

---
+++ spring-cloud-hystrix/hystrix-producer/src/main/java/com/fanshoufeng/producer/HystrixProducerApplication.java
@@ -0,0 +1,15 @@
+package com.fanshoufeng.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class HystrixProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixProducerApplication.class, args);
+    }
+
+}
