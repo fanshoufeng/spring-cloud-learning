2765e4ac69b860fbd71f8eb4fff3ed5c67b947a5
Michael Fan <shoufeng.fan@gmail.com>
2020-06-09 22:01:48 +0800

    新增hystrix-consumer

---
+++ spring-cloud-hystrix/hystrix-consumer/src/main/java/com/fanshoufeng/consumer/remote/HelloRemote.java
@@ -0,0 +1,13 @@
+package com.fanshoufeng.consumer.remote;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name= "hystrix-producer", fallback = HelloRemoteHystrix.class)
+public interface HelloRemote {
+
+    @RequestMapping(value = "/hello")
+    String hello(@RequestParam(value = "name") String name);
+
+}
---
+++ spring-cloud-hystrix/hystrix-consumer/src/test/java/com/fanshoufeng/consumer/HystrixConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanshoufeng.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
